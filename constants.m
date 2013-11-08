61368b5e550f2c414cde323682219884727b48c2
Shain <shainen@gmail.com>
2013-11-08 12:07:54 -0500

    fix

--- constants.m
+++ constants.m
@@ -8,12 +8,11 @@ Nt=128;
 dt=tmax/Nt;
 Length[ts=N@Range[0,tmax-dt,dt]];
 times=Table[t,{t,0,tmax-dt,dt}];
-Runs=100;
 
-su2Runs=100000;
-su3Runs=100000;
+su2Runs=100;
+su3Runs=100;
 
-mSIM=100;
+mSIM=20;
 
 Uvalue=1;
 \[Mu]value=1;
