e5a9eaeae934b078ac453596adf425778d21e203
Shain <shainen@gmail.com>
2013-11-09 10:31:35 -0500

    fix

--- constants.m
+++ constants.m
@@ -20,5 +20,5 @@ Jvalue=0.01;
 navgvalue=1;
 putvalues:={U->Uvalue,J->Jvalue,navg->navgvalue,\[Mu]->\[Mu]value};
 
-su2outfile="data/su2S"<>ToString[Sites]<>"t"<>ToString[tmax]<>"j"<>ToString[Jvalue]<>"r"<>ToString[su2Runs]<>".lst";
-su3outfile="data/su3S"<>ToString[Sites]<>"t"<>ToString[tmax]<>"j"<>ToString[Jvalue]<>"r"<>ToString[su3Runs]<>".lst";
+su2outfile="data/su2awS"<>ToString[Sites]<>"t"<>ToString[tmax]<>"j"<>ToString[Jvalue]<>"r"<>ToString[su2Runs]<>".lst";
+su3outfile="data/su3awS"<>ToString[Sites]<>"t"<>ToString[tmax]<>"j"<>ToString[Jvalue]<>"r"<>ToString[su3Runs]<>".lst";
