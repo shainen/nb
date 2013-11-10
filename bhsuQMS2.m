6d92a627678a5656bee588b80f44ad28f6962f25
Shainen Davidson <shainen@buphyg.bu.edu>
2013-11-10 14:30:55 -0500

    fix

--- bhsuQMS2.m
+++ bhsuQMS2.m
@@ -7,7 +7,7 @@ dt = tmax/Nt;
 
 t=[0:dt:tmax-dt];
 
-S = 3;
+S = 2;
 U = 1;
 mu = 1;
 J = 0.01;
@@ -50,26 +50,20 @@ cputime-time
 En=diag(Em);
 
 init=zeros(3^S,1);
-init(8,1)=1;
+init(3,1)=1;
 
 cn=Vn'*init;
-% m1=Vn'*(JzS{1}*Vn);
-% m2=Vn'*(JzS{2}*Vn);
-% m3=Vn'*(JzS{3}*Vn);
-m1=Vn'*(JxS{1}*Vn);
-m2=Vn'*(JxS{2}*Vn);
-m3=Vn'*(JxS{3}*Vn);
+m1=Vn'*(JzS{1}*Vn);
+m2=Vn'*(JzS{2}*Vn);
+%m3=Vn'*(JzS{3}*Vn);
 
 %m7=Vn'*(JzS{4}*Vn);
 %m8=Vn'*(JzS{5}*Vn);
 %m9=Vn'*(JzS{6}*Vn);
 
-% m4=Vn'*(JzS{1}*(JzS{1}*Vn));
-% m5=Vn'*(JzS{2}*(JzS{2}*Vn));
-% m6=Vn'*(JzS{3}*(JzS{3}*Vn));
-m4=Vn'*(JxS{1}*(JxS{1}*Vn));
-m5=Vn'*(JxS{2}*(JxS{2}*Vn));
-m6=Vn'*(JxS{3}*(JxS{3}*Vn));
+m4=Vn'*(JzS{1}*(JzS{1}*Vn));
+m5=Vn'*(JzS{2}*(JzS{2}*Vn));
+%m6=Vn'*(JzS{3}*(JzS{3}*Vn));
 
 
 'plot'
@@ -78,19 +72,19 @@ for j=1:Nt
     vec=exp(-i*t(j)*En).*cn;
     AvgSz1(j)=vec'*(m1*vec);
     AvgSz2(j)=vec'*(m2*vec);
-    AvgSz3(j)=vec'*(m3*vec);
+%    AvgSz3(j)=vec'*(m3*vec);
 %    AvgSz4(j)=vec'*(m7*vec);
 %    AvgSz5(j)=vec'*(m8*vec);
 %    AvgSz6(j)=vec'*(m9*vec);
     SqSz1(j)=vec'*(m4*vec);
     SqSz2(j)=vec'*(m5*vec);
-    SqSz3(j)=vec'*(m6*vec);
+%    SqSz3(j)=vec'*(m6*vec);
 end
 cputime-time
 
 widSz1=SqSz1-AvgSz1.*AvgSz1;
 widSz2=SqSz2-AvgSz2.*AvgSz2;
-widSz3=SqSz3-AvgSz3.*AvgSz3;
+%widSz3=SqSz3-AvgSz3.*AvgSz3;
 
 %save('QMAvgSz3','AvgSz1','AvgSz2','AvgSz3','-ascii');
 %save('QMWidSz3','widSz1','widSz2','widSz3','-ascii');
@@ -105,7 +99,7 @@ widSz3=SqSz3-AvgSz3.*AvgSz3;
 
  plot(t,AvgSz1,'b');
  plot(t,AvgSz2,'r');
- plot(t,AvgSz3,'y');
+% plot(t,AvgSz3,'y');
 
  % figure
  %hold on
@@ -118,5 +112,5 @@ figure
 % %plot(t,X(:,1),'r');
  plot(t,widSz1,'b');
  plot(t,widSz2,'r');
- plot(t,widSz3,'y');
+% plot(t,widSz3,'y');
 %legend('n for p=0');
