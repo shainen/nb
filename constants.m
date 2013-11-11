cc67df602c73a023b41aed36824e2d57c82a4add
Shain <shainen@gmail.com>
2013-11-10 23:50:43 -0500

    fix

--- constants.m
+++ constants.m
@@ -1,7 +1,7 @@
 (* ::Package:: *)
 
-Sites=2;
-updownmiddle={1,1,0};
+Sites=3;
+updownmiddle={1,1,1};
 
 tmax=200;
 Nt=128;
@@ -13,7 +13,7 @@ su2Runs=100000;
 su3Runs=100;
 
 Uvalue=1;
-\[Mu]value=1;
+\[Mu]value=0;
 Jvalue=0.01;
 navgvalue=1;
 putvalues:={U->Uvalue,J->Jvalue,navg->navgvalue,\[Mu]->\[Mu]value};
