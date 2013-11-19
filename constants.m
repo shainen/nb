efcceee5e0e7b5e012726c473dedb8602df339cd
Shain <shainen@gmail.com>
2013-11-19 12:21:03 -0500

    fix

--- constants.m
+++ constants.m
@@ -1,22 +1,25 @@
 (* ::Package:: *)
 
-Sites=3;
-updownmiddle={3,0,0};
+Sites=10;
+updownmiddle={10,0,0};
 
-tmax=200;
+tmax=10;
 Nt=128;
 dt=tmax/Nt;
 Length[ts=N@Range[0,tmax-dt,dt]];
 times=Table[t,{t,0,tmax-dt,dt}];
 
 su2Runs=100000;
-su3Runs=1000;
+su3Runs=100000;
 
 Uvalue=1;
-\[Mu]value=0;
+\[Mu]value=1;
 Jvalue=0.01;
 navgvalue=1;
 putvalues:={U->Uvalue,J->Jvalue,navg->navgvalue,\[Mu]->\[Mu]value};
+
+Jx=0.1;
+Jy=0.2;
 
 su2outfile="data/su2awS"<>ToString[Sites]<>"t"<>ToString[tmax]<>"j"<>ToString[Jvalue]<>"r"<>ToString[su2Runs];
 su3outfile="data/su3awS"<>ToString[Sites]<>"t"<>ToString[tmax]<>"j"<>ToString[Jvalue]<>"r"<>ToString[su3Runs];
